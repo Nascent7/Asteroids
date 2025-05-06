0c3e29b8b2ee43c7c38c81d287563bf767fbedd2
Nascent7 <mahquintana@gmail.com>
2025-05-05 22:22:11 -0400

    Inital commit: creates main.py and req. fot pygame version

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+
+
+def main():
+    print("Starting Asteroids!")
+
+# not part of main function - "This line ensures the main() function is only called when this file is 
+# run directly; it won't run if it's imported as a module. 
+# It's considered the "pythonic" way to structure an executable program in Python. 
+# Technically, the program will work fine by just calling main(), 
+# but you might get an angry letter from Guido van Rossum if you don't."
+if __name__ == "__main__": 
+    main()
